885fe61c062aec438046259e8eb1bfa5a9de44a6
CesarCM19 <cesarcamposmora04@gmail.com>
2024-04-24 19:06:47 -0600

    ClaseCésarAct

--- ClaseCésar.java
+++ ClaseCésar.java
@@ -3,5 +3,12 @@ import javax.swing.JOptionPane;
 public class ClaseCésar {
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null,"Bienvenidos a mi clase");
+        int Contador =0;
+        while(Contador<=5){
+            System.out.println(Contador);
+            Contador++;
+        }
+        JOptionPane.showMessageDialog(null,"El contador llegó a 5");
+
     }
 }
